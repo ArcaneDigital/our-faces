23f16e780b7e6eb9f8cb6ef9a777d68aa50537ea
Max Dubinovsky <m.dubinovsky@arcane.ws>
2015-05-18 13:17:05 -0400

    Router didn't commit
    Committing the router

---
+++ server/staff/router.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var express         = require('express'),
+    app             = express(),
+    router          = express.Router(),
+    StaffCtrl       = require('./controller');
+
+
+
+
+
+router.route('/')
+
+    // Get list of staff members
+    .get(StaffCtrl.getMany);
+
+router.route('/:id(\\d+)/')
+
+    // Get staff member by ID
+    .get(StaffCtrl.getOne)
+
+    // Update staff member by ID
+    .post(StaffCtrl.update)
+
+    // Remove staff member by ID
+    .delete(StaffCtrl.delete);
+
+app.use('/staff', router);
+
+module.exports = app;
